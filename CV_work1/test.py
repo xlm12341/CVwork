adde94c81d2a590e9fa2d32d95cedcf21ee8d940
xugarxu <2793821834@qq.com>
2022-05-17 00:23:02 +0800

    work2 start

--- CV_work1/test.py
+++ CV_work1/test.py
@@ -2,17 +2,10 @@ import cv2
 import numpy as np
 import dlib
 import utils
-out = "1.mp4"
-fps = 20
-fourcc = cv2.VideoWriter_fourcc(*"MJPG")  # 支持jpg
-videoWriter = cv2.VideoWriter(out, fourcc, fps, (640, 480))
-cap = cv2.
-while True:
 
-for im_name in range(len(im_names) - 2):
-    string = img_root + 'frame' + str(im_name) + '.jpg'
-    print(string)
-    frame = cv2.imread(string)
-    frame = cv2.resize(frame, (640, 480))
-    videoWriter.write(frame)
+cap = cv2.imread("img_repo/01_background.jpg")
 
+img2 = cv2.resize(cap,(640, 480),interpolation=cv2.INTER_LINEAR)
+cv2.imshow("s", img2)
+cv2.imwrite("../CV_work2/img_repo/01_background.jpg", img2)
+print("done")
---
+++ CV_work2/background_rm.py
@@ -0,0 +1,39 @@
+from cvzone.SelfiSegmentationModule import SelfiSegmentation
+import cv2
+import cvzone
+import mediapipe
+import numpy
+import os
+cap = cv2.VideoCapture(0)
+seg = SelfiSegmentation()
+fps_reader = cvzone.FPS()
+bk_img = cv2.imread("img_repo/01_background2.jpg")
+dir_list = os.listdir("img_repo")
+bk_img_list = []
+
+for name in dir_list:
+    img_tmp = cv2.imread(f"img_repo/{name}")
+    print(img_tmp.shape)
+    bk_img_list.append(img_tmp)
+
+
+bk_img_index = 0
+while True:
+    success, img = cap.read()
+    img_p = seg.removeBG(img, bk_img_list[bk_img_index], threshold=0.4)
+
+    stacked_img = cvzone.stackImages([img, img_p], 2, 1)
+    _, stacked_img = fps_reader.update(stacked_img)
+    cv2.imshow("img", stacked_img)
+
+    key = cv2.waitKey(1)
+    if key == ord('a'):
+        if bk_img_index > 0:
+            bk_img_index = bk_img_index -1
+    elif key == ord('d'):
+        if bk_img_index < len(bk_img_list) - 1:
+            bk_img_index = bk_img_index + 1
+    elif key == ord('q'):
+        break
+
+exit(0)
